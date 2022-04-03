a4cc3a3de2c5915144c4a74d39eb84e8efb503b4
zoirovbahrom <zoirovbakhrom10@gmail.com>
2022-04-03 10:55:05 +0500

    text

---
+++ main.js
@@ -0,0 +1,30 @@
+let a = prompt("Oy raqamini kiriting..");
+if (a == 1) {
+    document.write("Qish");
+} else if (a == 2) {
+    document.write("Qish");
+}
+else if (a == 3) {
+    document.write("Bahor");
+} else if (a == 4) {
+    document.write("Bahor");
+} else if (a == 5) {
+    document.write("Bahor");
+} else if (a == 6) {
+    document.write("Yoz");
+} else if (a == 7) {
+    document.write("Yoz");
+} else if (a == 8) {
+    document.write("Yoz");
+} else if (a == 9) {
+    document.write("Kuz");
+} else if (a == 10) {
+    document.write("Kuz");
+} else if (a == 11) {
+    document.write("Kuz");
+} else if (a == 12) {
+    document.write("Qish");
+
+} else {
+    document.write("Bunday fasl mavjud amas")
+}
